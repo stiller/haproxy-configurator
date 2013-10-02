fb25d7074533e4bf8e2a6acc7165e13f142fb34f
Joachim Nolten <joachim@nolten.org>
2013-10-02 11:39:26 +0200

    Works as require and cli command. Gets domains from server.

--- haproxy_config.rb
+++ haproxy_config.rb
@@ -3,18 +3,18 @@ require 'yaml'
 require 'net/https'
 require 'uri'
 require 'json'
-require 'pry'
+require 'fileutils'
 
 CONFPATH = Dir.home
 CONFFILE = 'proxyconf.yml'
 
 class HAProxyConfig
-  attr_accessor :config, :domains, :request, :http
+  attr_reader :config
 
   def initialize
     if File.exists?(CONFPATH + "/" + CONFFILE)
-      config = YAML.load_file(CONFPATH + "/" + CONFFILE)
-      config.default_proc = proc{ |h,k| h.key?(k.to_s) ? h[k.to_s] : nil }
+      @config = YAML.load_file(CONFPATH + "/" + CONFFILE)
+      @config.default_proc = proc{ |h,k| h.key?(k.to_s) ? h[k.to_s] : nil }
     else
       raise "Config file not found: #{CONFPATH}/#{CONFFILE}"
     end
@@ -25,16 +25,31 @@ class HAProxyConfig
     @request.basic_auth(config[:user],config[:password])
   end
 
-  def get
+  def fetch
     response = @http.request(@request)
-    JSON.parse response.body
+    @domains = JSON.parse response.body
+    self
   end
 
-  def create
-    template_file = File.open("haproxy.cfg.erb", 'r').read
-    erb = ERB.new(template_file)
-    File.open("/etc/haproxy/haproxy.cfg", 'w') { |file| file.write(erb.result(binding)) }
+  def render template_file = "haproxy.cfg.erb"
+    template = File.open(template_file, 'r').read
+    erb = ERB.new(template)
+    @result = erb.result(binding)
+    self
   end
-
+  
+  def write config_path = "/etc/haproxy/haproxy.cfg"
+    return unless File.exists?(config_path)
+    FileUtils.cp(config_path, config_path.split(".")[0] + ".bk")
+    File.open(config_path, 'w') { |file| file.write(@result) }
+    puts "Done."
+  end
 end
-puts HAProxyConfig.new.get
+
+if $stdout.tty? # execute if called from terminal
+  if ENV["USER"] == "root"
+    HAProxyConfig.new.fetch.render.write
+  else
+    puts "Please call using sudo."
+  end
+end
